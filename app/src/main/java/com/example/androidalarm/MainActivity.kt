7a57a6b073dd7ed28a193460b6714961326830ab
rmiskiewicz <rmiskiewicz@corelogic.pl>
2021-09-30 14:34:02 +0200

    usunięcie tekstu na dole ekranu

--- app/src/main/java/com/example/androidalarm/MainActivity.kt
+++ app/src/main/java/com/example/androidalarm/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.example.androidalarm
 
 import android.os.Bundle
-import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidalarm.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val NO_ALARM_TEXT = "Nenhum alarme agendado"
-    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -32,22 +26,12 @@ class MainActivity : AppCompatActivity() {
     private fun scheduleAlarm(seconds: Long) {
         val alarm = Calendar.getInstance()
         alarm.add(Calendar.SECOND, seconds.toInt())
-        val time = SimpleDateFormat("HH:mm:ss dd-MM-yyyy").format(alarm.time)
         AlarmHelper().startAlarmManager(this, seconds)
-        val text = "Alarme programado para as $time"
-        binding.alarmScheduledTo.text = text
     }
 
     private fun cancelAlarms() {
         AlarmPlayer.instance.stop()
-        setText(NO_ALARM_TEXT)
         NotificationHelper().dismissNotification(this)
         AlarmHelper().stopAlarmManager(this)
-    }
-
-    private fun setText(text: String?) {
-        if (!TextUtils.isEmpty(text)) {
-            binding.alarmScheduledTo.text = text
-        }
     }
 }
