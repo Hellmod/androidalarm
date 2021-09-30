8360f13cb2faf148562baf6b100a609cf0e10504
rmiskiewicz <rmiskiewicz@corelogic.pl>
2021-09-30 14:16:25 +0200

    czyszczenie

--- app/src/main/java/com/example/androidalarm/MainActivity.kt
+++ app/src/main/java/com/example/androidalarm/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.androidalarm
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidalarm.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
@@ -12,7 +10,6 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        private const val TIME_PICKER_ACTIVITY_ID = 1234
         private const val NO_ALARM_TEXT = "Nenhum alarme agendado"
         private const val SHARED_PREFERENCE_NAME = "AlarmText"
     }
@@ -23,52 +20,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.favoriteSchedule1.setOnClickListener {
-            scheduleAlarm(5)
-        }
-
-        binding.favoriteSchedule2.setOnClickListener {
-            scheduleAlarm(30)
-        }
-
-        binding.chooseTime.setOnClickListener {
-            startActivityForResult(
-                Intent(this, TimePickerActivity::class.java),
-                TIME_PICKER_ACTIVITY_ID
-            )
-        }
 
         binding.cancelButton.setOnClickListener {
             cancelAlarms()
         }
 
+        binding.okButton.setOnClickListener{
+            scheduleAlarm(binding.timeText.text.toString().toLong())
+        }
+
         reloadPreference()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == TIME_PICKER_ACTIVITY_ID) {
-            if (resultCode == RESULT_OK) {
-                if (data != null) {
-                    manageTimePickerResult(data)
-                }
-            }
-        }
-    }
-
-    private fun manageTimePickerResult(data: Intent) {
-        val hour = data.getIntExtra(TimePickerActivity.RESULT_HOUR, -1)
-        val minute = data.getIntExtra(TimePickerActivity.RESULT_MINUTE, -1)
-
-        if (hour == -1 || minute == -1) {
-            Toast.makeText(
-                this,
-                "Ops! Isso é vergonhoso! Não consegui ler os dados que eu mesmo passei! #chatiado",
-                Toast.LENGTH_LONG
-            ).show()
-            return
-        }
-        scheduleAlarm(TimeConverter().toSeconds(hour, minute, 0))
     }
 
     private fun scheduleAlarm(seconds: Long) {
